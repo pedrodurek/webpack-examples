f179b1d3a4892b7ee7cd78e8c4a5bb6dfad7fc6b
Pedro Durek <pedro.durek@shopify.com>
2021-03-28 22:03:31 -0500

    Update example

--- app1/src/index.tsx
+++ app1/src/index.tsx
@@ -1,10 +1,15 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App: FC = () => (
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-);
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Button</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));

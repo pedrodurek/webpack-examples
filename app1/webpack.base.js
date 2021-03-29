c69a8e77fc6bc51c5e3066aa97870dd986ff016c
Pedro Durek <pedro.durek@shopify.com>
2021-03-28 21:42:38 -0500

    Add initial setup for htmlwebpack plugin

--- app1/webpack.base.js
+++ app1/webpack.base.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   output: {
@@ -14,4 +15,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };

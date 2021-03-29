9b8b8244dbb35429c80ef54e612406a54a538c0f
Pedro Durek <pedro.durek@shopify.com>
2021-03-28 21:29:37 -0500

    Add initial react scaffold

---
+++ app1/src/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import ReactDOM from 'react-dom';
+
+const App: FC = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- app1/webpack.base.js
+++ app1/webpack.base.js
@@ -8,7 +8,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },

760e0a0d84e68be4c5bac2203a1317dc04cee8a3
Pedro Durek <pedro.durek@shopify.com>
2021-04-04 22:24:10 -0500

    Fix route to rely on history api

--- app1/webpack.dev.js
+++ app1/webpack.dev.js
@@ -1,9 +1,11 @@
 const base = require('./webpack.base.js');
 const { merge } = require('webpack-merge');
+// const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = merge(base, {
   mode: 'development',
   devServer: {
+    historyApiFallback: true,
     port: 3000,
     publicPath: '/',
     hot: true,
@@ -12,4 +14,13 @@ module.exports = merge(base, {
   entry: {
     app: './src/index.tsx',
   },
+  // plugins: [
+  //   new ModuleFederationPlugin({
+  //     name: 'app1',
+  //     exposes: {
+  //       Dashboard: './src/views/Dashboard',
+  //     },
+  //     shared: ['react', 'react-dom', 'react-router-dom'],
+  //   }),
+  // ],
 });

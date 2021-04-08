9e6b660f5c0887489f8613685a85878cadf49f62
Pedro Durek <pedro.durek@shopify.com>
2021-04-07 22:42:38 -0500

    Turn entry point asynchronous

---
+++ app1/src/App.tsx
@@ -0,0 +1,30 @@
+import { FC, lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+const Dashboard = lazy(
+  () => import(/* webpackChunkName: "dashboard" */ './views/Dashboard'),
+);
+
+const App: FC = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route
+            exact
+            path="/"
+            render={() => <Redirect from="/" to="/dashboard" />}
+          />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
--- app1/src/index.tsx
+++ app1/src/index.tsx
@@ -1,31 +1,7 @@
-import React, { FC, lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-
-const Dashboard = lazy(
-  () => import(/* webpackChunkName: "dashboard" */ './views/Dashboard'),
-);
-
-const App: FC = () => {
-  return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path="/dashboard" component={Dashboard} />
-          <Route
-            exact
-            path="/"
-            render={() => <Redirect from="/" to="/dashboard" />}
-          />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('app'));
+/**
+ * As we are exposing some modules via module federation, they become asynchronously.
+ * That means they are not ready on the initial render, which returns the following error:
+ * Uncaught Error: Shared module is not available for eager consumption
+ * In order to sort it out, we have to turn the entry point asynchronous as well.
+ *  */
+import('./bootstrap');

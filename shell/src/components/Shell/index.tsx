7976bb80c02282df9433290bfd632cbf8362611e
Pedro Durek <pedro.durek@shopify.com>
2021-04-23 18:30:11 -0500

    Import remote component

--- shell/src/components/Shell/index.tsx
+++ shell/src/components/Shell/index.tsx
@@ -1,11 +1,35 @@
-import Header from '../../components/Header';
+import React, { lazy, Suspense } from 'react';
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+import Header from '../Header';
 import { RootContainer } from './styles';
+
+const Dashboard = lazy(
+  () => import(/* webpackChunkName: "dashboard" */ 'app1/Dashboard'),
+);
 
 const Shell = () => {
   return (
     <RootContainer>
       <Header />
-      <main></main>
+      <main>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+              <Route
+                exact
+                path="/"
+                render={() => <Redirect to="/dashboard" />}
+              />
+            </Switch>
+          </Suspense>
+        </Router>
+      </main>
     </RootContainer>
   );
 };
